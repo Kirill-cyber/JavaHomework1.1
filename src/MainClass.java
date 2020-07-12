a414d99847fc2a6120d41db2fed1819a9d1c3b61
Kirill Khoroshevskiy <ceasarrr12@gmail.com>
2020-07-12 19:33:50 +0300

    initial commit

---
+++ src/MainClass.java
@@ -0,0 +1,22 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainClass {
+
+    @Test
+    public void testGetLocalNumber()
+    {
+        Assert.assertEquals("Incorrect value has returned from getLocalNumber", 14, this.getLocalNumber();
+    }
+
+
+
+
+    public int getLocalNumber()
+    {
+        return 14;
+
+    }
+}
